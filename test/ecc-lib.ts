7d3a925680a499e95dd3bf8691e6b6d7cfc1ba46
kstasi <nastasiakondaurova@gmail.com>
2020-08-08 20:17:21 +0300

    point interface updated

--- test/ecc-lib.ts
+++ test/ecc-lib.ts
@@ -30,46 +30,61 @@ describe("hello world contract test suite", () => {
 
     it("should return valid result for typical cases", async () => {
       const query = ellipticCurveClient.createQuery({
-        method: { name: "ecc-add", args: ["1", "2", "3", "4"] },
+        method: {
+          name: "ecc-add",
+          args: ["(tuple (x 1) (y 2))", "(tuple (x 3) (y 4))"],
+        },
       });
       const receipt = await ellipticCurveClient.submitQuery(query);
       const result = Result.unwrap(receipt);
-      assert.equal(result, "(ok (-3 2))");
+      assert.equal(result, "(ok (tuple (x -3) (y 2)))");
     });
 
     it("should return valid result for tangent", async () => {
       const query = ellipticCurveClient.createQuery({
-        method: { name: "ecc-add", args: ["-1", "4", "1", "2"] },
+        method: {
+          name: "ecc-add",
+          args: ["(tuple (x -1) (y 4))", "(tuple (x 1) (y 2))"],
+        },
       });
       const receipt = await ellipticCurveClient.submitQuery(query);
       const result = Result.unwrap(receipt);
-      assert.equal(result, "(ok (1 -2))");
+      assert.equal(result, "(ok (tuple (x 1) (y -2)))");
     });
 
     it("should return valid result for equal points", async () => {
       const query = ellipticCurveClient.createQuery({
-        method: { name: "ecc-add", args: ["2", "1", "2", "1"] },
+        method: {
+          name: "ecc-add",
+          args: ["(tuple (x 2) (y 1))", "(tuple (x 2) (y 1))"],
+        },
       });
       const receipt = await ellipticCurveClient.submitQuery(query);
       const result = Result.unwrap(receipt);
-      assert.equal(result, "(ok (32 -181))");
+      assert.equal(result, "(ok (tuple (x 32) (y -181)))");
     });
 
     it("should return 0 0 in case of equal points and y1=y2=0", async () => {
       const query = ellipticCurveClient.createQuery({
-        method: { name: "ecc-add", args: ["1", "0", "1", "0"] },
+        method: {
+          name: "ecc-add",
+          args: ["(tuple (x 1) (y 0))", "(tuple (x 1) (y 0))"],
+        },
       });
       const receipt = await ellipticCurveClient.submitQuery(query);
       const result = Result.unwrap(receipt);
-      assert.equal(result, "(ok (0 0))");
+      assert.equal(result, "(ok (tuple (x 0) (y 0)))");
     });
     it("should return 0 0 in case of different points and x1=x2", async () => {
       const query = ellipticCurveClient.createQuery({
-        method: { name: "ecc-add", args: ["1", "2", "1", "4"] },
+        method: {
+          name: "ecc-add",
+          args: ["(tuple (x 1) (y 2))", "(tuple (x 1) (y 4))"],
+        },
       });
       const receipt = await ellipticCurveClient.submitQuery(query);
       const result = Result.unwrap(receipt);
-      assert.equal(result, "(ok (0 0))");
+      assert.equal(result, "(ok (tuple (x 0) (y 0)))");
     });
   });
 
