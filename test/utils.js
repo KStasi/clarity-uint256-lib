813addfb8a955ea1e2cc7911a245b3d39bd7c2b0
kstasi <nastasiakondaurova@gmail.com>
2020-08-10 09:27:13 +0300

    tuple convertor fixed

--- test/utils.js
+++ test/utils.js
@@ -1,12 +1,11 @@
-
 exports.hexToUint256 = (str) => {
-    var re = /[0-9A-Fa-f]{64}/g;
-    str = str.replace(/$0x/, '');
-    str = str.padStart(64, '0');
-    if (!re.test(str)) throw "Invalid hex string";
-    let parts = [];
-    for (let i = 0; i < 64; i += 16) {
-        parts.push(parseInt(str.slice(i, i + 16), 16))
-    }
-    return `(tuple (i0 u${parts[0]}) (i1 u${parts[1]}) (i2 u${parts[2]}) (i3 u${parts[3]}))`;
-}
+  var re = /[0-9A-Fa-f]{64}/g;
+  str = str.replace(/$0x/, "");
+  str = str.padStart(64, "0");
+  if (!re.test(str)) throw "Invalid hex string";
+  let parts = [];
+  for (let i = 0; i < 64; i += 16) {
+    parts.push(BigInt("0x" + str.slice(i, i + 16)));
+  }
+  return `(tuple (i0 u${parts[0]}) (i1 u${parts[1]}) (i2 u${parts[2]}) (i3 u${parts[3]}))`;
+};
